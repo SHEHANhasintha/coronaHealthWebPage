f266787289f2006c6235dd1c235d09824e68b3d1
shehan <hasinthashehan768@gmail.com>
2020-04-20 04:26:33 +0530

    frontbackserver

--- covidatlas/src/App.js
+++ covidatlas/src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          CovidAtlas - we will care about your medicine!
-        </p>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = { users: [] }
+
+
+  componentDidMount(){
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+
+  };
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            CovidAtlas - we will care about your medicine!
+          </p>
+          <ul>
+            {this.state.users.map(user => 
+              <li key={user.id}>{user.username}</li>)}
+          </ul>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
