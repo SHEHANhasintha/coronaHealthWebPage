e53b4f9cfdfc54da71214e85bae66833037ff18c
shehan <hasinthashehan768@gmail.com>
2020-06-20 18:56:27 +0530

    fix 5

--- covidatlas/src/thirdParty/FacebookLogin/FacebookLogin.js
+++ covidatlas/src/thirdParty/FacebookLogin/FacebookLogin.js
@@ -29,8 +29,25 @@ class FacebookLogin extends Component{
 	      console.log(thita,process.env.REACT_APP_APPLICATION_PROXY+ "/auth/local");*/
 	      axios
 	        .post(`http://localhost:5000/auth${context.loc}/facebook`,thita)
-	        .then((res) => console.log(res))
-	        .catch((err) => console.log(err))
+          .then((res) => {
+            console.log(res)
+            if (res.status == 200){
+              //toggleAuth(true)
+              localStorage.setItem('email', res.data.login.email);
+              localStorage.setItem('firstName', res.data.login.firstName);
+              localStorage.setItem('lastName', res.data.login.lastName);
+              localStorage.setItem('token', res.data.tokenData.token);
+              localStorage.setItem('iat', res.data.tokenData.iat);
+              localStorage.setItem('exp', res.data.tokenData.exp);
+              localStorage.setItem('isAuthenticated', true);
+
+              props.history.push('/app')
+            }
+          })
+          .catch((err) => {
+            console.log(err)
+            //updateEmailFail(true);
+            })
 	      //await resolve(cb());
 	    }))
 	}
--- covidatlas/src/thirdParty/GoogleLogin/GoogleLogin.js
+++ covidatlas/src/thirdParty/GoogleLogin/GoogleLogin.js
@@ -19,8 +19,25 @@ class FacebookLogin extends Component{
 	      thita.email = res.profileObj.email;
 	      axios
 	        .post(`http://localhost:5000/auth${context.loc}/google`,thita)
-	        .then((res) => console.log(res))
-	        .catch((err) => console.log(err))
+          .then((res) => {
+            console.log(res)
+            if (res.status == 200){
+              //toggleAuth(true)
+              localStorage.setItem('email', res.data.login.email);
+              localStorage.setItem('firstName', res.data.login.firstName);
+              localStorage.setItem('lastName', res.data.login.lastName);
+              localStorage.setItem('token', res.data.tokenData.token);
+              localStorage.setItem('iat', res.data.tokenData.iat);
+              localStorage.setItem('exp', res.data.tokenData.exp);
+              localStorage.setItem('isAuthenticated', true);
+
+              props.history.push('/app')
+            }
+          })
+          .catch((err) => {
+            console.log(err)
+            updateEmailFail(true);
+            })
 	    }))
 
 	}
