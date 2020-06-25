b7f873faf45bd9f8516cc3907b56dd2182b72648
shehan <hasinthashehan768@gmail.com>
2020-06-25 10:11:43 +0530

    fixed google

--- covidatlas/src/thirdParty/GoogleLogin/GoogleLogin.js
+++ covidatlas/src/thirdParty/GoogleLogin/GoogleLogin.js
@@ -22,31 +22,28 @@ class GoogleLogin extends Component{
 	      console.log(thita);	
 
 
-      postData(`/auth${context.loc}/google`,thita)
-        .then((res) => {
-          console.log(res);
-
-
-            if (res.status == 'success'){
-              localStorage.setItem('email', res.login.email);
-              localStorage.setItem('firstName', res.login.firstName);
-              localStorage.setItem('lastName', res.login.lastName);
-              localStorage.setItem('token', res.tokenData.token);
-              localStorage.setItem('iat', res.tokenData.iat);
-              localStorage.setItem('exp', res.tokenData.exp);
+        console.log(thita);
+        axios
+          .post(`/auth${context.loc}/google`,thita)
+          .then((res) => {
+            console.log(res)
+            if (res.status == 200){
+              //toggleAuth(true)
+              localStorage.setItem('email', res.data.login.email);
+              localStorage.setItem('firstName', res.data.login.firstName);
+              localStorage.setItem('lastName', res.data.login.lastName);
+              localStorage.setItem('token', res.data.tokenData.token);
+              localStorage.setItem('iat', res.data.tokenData.iat);
+              localStorage.setItem('exp', res.data.tokenData.exp);
               localStorage.setItem('isAuthenticated', true);
               console.log(this.props);
               this.props.props.history.push('/app')
-            }else{
-            	console.log(res,'resss');
             }
-
-
-        })
-        .catch((err) => {
-          console.log(err)
-          updateEmailFail(true);
-        })
+          })
+          .catch((err) => {
+            console.log(err)
+            //updateEmailFail(true);
+            })
 
 
 
